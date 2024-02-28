a5b3e1bfc7dafd40a8a33d6460db3f8c3f148afd
ractodev <erik.j.winbladh@gmail.com>
2024-02-28 11:45:09 +0100

    Mockstructure of unit test for getNonFabricMods

---
+++ src/test/java/net/fabricmc/loader/impl/discovery/GetNonFabricModsTest.java
@@ -0,0 +1,53 @@
+package net.fabricmc.loader.impl.discovery;
+
+import net.fabricmc.loader.impl.FabricLoaderImpl;
+import net.fabricmc.loader.impl.game.GameProvider;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import static org.mockito.Mockito.mock;
+
+public class GetNonFabricModsTest {
+	private FabricLoaderImpl loader;
+	private ModDiscoverer discoverer;
+
+
+
+	@BeforeEach
+	public void setUp() {
+		GameProvider provider = mock();
+		Mockito.when(provider.getBuiltinMods()).thenReturn(Collections.emptyList());
+		loader = mock();
+		Mockito.when(loader.getGameProvider()).thenReturn(provider);
+		Mockito.when(loader.isDevelopmentEnvironment()).thenReturn(false);
+		discoverer = new ModDiscoverer(mock(), mock());
+		discoverer.addCandidateFinder(new MockCandidateFinder());
+	}
+
+	@Test
+	public void testGetNonFabricMods() throws ModResolutionException {
+		discoverer.discoverMods(loader, new HashMap<String, Set<ModCandidate>>());
+		Set<ModCandidate> nonFabricMods = discoverer.getNonFabricMods();
+	}
+
+	public static class MockCandidateFinder implements ModCandidateFinder {
+		@Override
+		public void findCandidates(ModCandidateConsumer out) {
+			List<Path> modPaths = new ArrayList<>();
+			modPaths.add(Paths.get("./src/test/resources/testing.discovery/dummyFabricMod.jar"));
+			modPaths.add(Paths.get("./src/test/resources/testing.discovery/dummyNonFabricMod.jar"));
+			out.accept(modPaths, false);
+		}
+	}
+
+}
